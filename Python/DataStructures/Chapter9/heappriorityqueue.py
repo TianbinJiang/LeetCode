98158b2ccea11f36814b6c6f30c6061c2a594820
Tianbin Jiang <jiang@cs.queensu.ca>
2015-05-24 13:34:27 -0400

    priority queue base added

---
+++ Python/DataStructures/Chapter9/heappriorityqueue.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+from priorityqueuebase import PriorityQueueBase
+
+class HeapPriorityQueue(PriorityQueueBase):
+	def _parent(self, j):
+		return (j - 1) // 2
+
+	def _left(self, j):
+		return 2 * j + 1
+
+	def _right(self, j):
+		return 2 * j + 2
+
+	def _has_left(self, j):
+		return self._left(j) < len(self._data)
+
+	def _has_right(self, j):
+		return self._right(j) < len(self._data)
+
+	def _swap(self, i, j):
+		self._data[i], self._data[j] = self._data[j], self._data[i]
+
+	def _upheap(self, j):
+		parent = self._parent(j)
+		if j > 0 and self._data[j] < self._data[parent]:
+			self._swap(j, parent)
+			self._upheap(parent)
+
+	def _downheap(self, j):
+		if self._has_left(j):
+			left = self._left(j)
+			small_child  = left
+			if self._has_right(j):
+				right = self._right(j)
+				if self._data[right] < self._data[left]:
+					small_child = right
+			if self._data[small_child] < self._data[j]:
+				self._swap(j, small_child)
+				self._downheap(small_child)
+
+#-------public behaviors----------#
+
+	def __init__(self):
+		self._data = []
+		
+	def __len__(self):
+		return len(self._data)
+
+	def add(self, key, value):
+		self._data.append(self._item(key, value))
+		self._update(len(self._data) - 1)
+
+	def min(self):
+		if self.is_empty():
+			raise Empty('Priority queue is empty')
+		item = self._data[0]
+		return (item._key, item._value)
+
+	def remove_min(self):
+		if self.is_empty():
+			raise Empty('Priority queue is empty')
+		self._swap(0, len(self._data) - 1)
+		item = self._data.pop()
+		self._downheap(0)
+		return (item._key, item._value)
+
+
+	def __init__(self, contents = ()):
+		self._data = [self._Item(k, v) for k, v in contents]
+		if len(self._data) > 1:
+			self._heapify()
+
+	def _heapify(self):
+		start = self._parent(len(self) - 1)
+		for j in range(start, -1, -1):
+			self._downheap(j)
+
+
+#--------Queue Sort---------#
+def pq_sort(C):
+	n = len(C)
+	P = PriorityQueue()
+	for j in range(n):
+		element = C.delete(C.first)
+		P.add(element, element)
+	for j in range(n):
+		(k,v) = P.remove_min()
+		C.add_last(v)
