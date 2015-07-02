8c22c2a2b43a0718f79e9f5a8510884ae78e3480
Tianbin Jiang <jiang@cs.queensu.ca>
2015-07-02 12:42:09 -0400

    matrix maximun subset

--- CrackingCodeInterview/20-12.cc
+++ CrackingCodeInterview/20-12.cc
@@ -35,7 +35,42 @@ int LargestSum(vector< vector<int> > &matrix){
 
 // Optimized Method. O(n ^ 4), we can precompute some subset.
 
+// This method is provided by hawstein. Pre-compute a matrix sum subset table.
+// We use a 2-D matrix to keep the subset sum. p[i][j] stands for from (i, j) to (1, 1) subset sum.
+// p[i][j] = p[i-1][j] + p[i][j- 1] - p[i-1][j-1] + A[i][j]. A[i][j] stands for matrix element (i, j);
 
+const int MAX_N = 100;
+int p[MAX_N][MAX_N];
+
+void preCompute(vector< vector<int> > &matrix){
+	n = matrix.size();
+	for(int i = 0; i<= n; ++i)
+		p[0][i] = p[i][0] = 0;
+	for(int i = 1; i <= n; ++i){
+		for(int j = 1; j <= n; ++j){
+			p[i][j] = p[i-1][j] + p[i][j-1] - p[i-1][j-1] + matrix[i][j]
+		}
+	}
+}
+
+int MaxSum(vector< vector<int> > &matrix){
+	int max_sum = 1 << 31; //min_int;
+	for(int i = 1; i < n; ++i){
+		for(int j = 1; j < n; ++j){
+			int cur_sum = 0;
+			for(int k = 1; k <= n; ++k){
+				int val = p[j][k] - p[j][k-1] - p[i - 1][k] + p[i-1][k-1];
+				if(cur_sum < 0)
+						cur_sum = val;
+				else
+						cur_sum += val;
+				if(cur_sum > max_sum)
+					max_sum = cur_sum;
+			}
+		}
+	}
+	return max_sum;
+}
 
 /*---TEST---*/
 int main(void){
